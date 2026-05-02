e9983b29368c9d2a303322ca6675e32f2135e2a3
minninna <minninna@gmail.com>
2026-05-02 08:50:39 +0200

    v105

---
+++ new/assets/photos-latest.js
@@ -0,0 +1,14 @@
+const latestShots = {
+  titles: [
+    "Single",
+    "Mona Lisa",
+    "She's A Rainbow",
+    "Leave A Light On",
+    "Time",
+    "Workin’ Man Blues",
+    "Don’t Leave Me This Way",
+    "How Sweet It Is (To Be Loved By You)",
+    "Nothing's Gonna Change My Love",
+    "Atomic"
+  ]
+};
